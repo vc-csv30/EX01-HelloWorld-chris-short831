2068ccbf2f694aa00b7c85d18ebebb62619c762d
chrisshort <chris.short831@gmail.com>
2019-09-11 13:38:54 -0700

    I have committed to the beginning of the end of my life as I once knew it.

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,11 @@
-//
-// Created by aknight on 8/2/19.
+//I hereby state this is my own work that I copied from Allan Knight
+#include <iostream>
+#include <csv30>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
+// Created by CShort on 9/11/19.
 //
 
