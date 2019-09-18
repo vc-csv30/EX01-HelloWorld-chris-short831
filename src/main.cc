85f7c06ed5511faac8f689cfa506eec87488f57b
Cr055-CPU <chavezchristian17@yahoo.com>
2019-09-18 11:29:00 -0700

    I commit to never messing with the git sources file so i can turn my assignments in on time.

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,7 @@
  * Statement of code ownership: I hereby state that I have written all of this
  * code and I have not copied this code from any other person or source.
  *
- * @author [CHANGE THIS INFORMATION]
+ * @Chris Short [chris.short831@gmail.com]
  */
  
 #include <iostream>
